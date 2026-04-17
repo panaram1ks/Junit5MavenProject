e5a5a450bc19af1850393b40f425a4dbd47e2187
parom <E.Parominskiy@isida.by>
2026-04-17 10:00:15 +0300

    12. Add JUnit Dependencies

---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DemoTest {
+
+    @DisplayName("Junit is normal add to project")
+    @Test
+    void chekJunitLibs(){
+        System.out.println("JunitIsWorkNormal");
+    }
+
+}
