11cc55bc9f5c917cbb66cdcf629644e9b7033355
parom <parom.evgen@mail.ru>
2026-04-20 17:42:27 +0300

    31. Lifecycle methods demo

--- src/test/java/com/parom/CalculatorTest.java
+++ src/test/java/com/parom/CalculatorTest.java
@@ -1,18 +1,40 @@
 package com.parom;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 @DisplayName("Test Math operations in Calculator class")
 class CalculatorTest {
+
+    Calculator calculator;
+
+    @BeforeAll
+    static void setup(){
+        System.out.println("Execute before All method");
+    }
+
+    @AfterAll
+    static void cleanup(){
+        System.out.println("Execute After all method");
+    }
+
+    @BeforeEach
+    void beforeEachTestMethod(){
+        System.out.println("Execute Before Each method");
+        this.calculator = new Calculator();
+    }
+
+    @AfterEach
+    void afterEachTestMethod(){
+        System.out.println("Execute After Each method");
+    }
 
     @DisplayName("Test 4/2 = 2")
     // test<System Under Test>_<Condition or State Change>_<Expected Result>
     @Test
     void testIntegerDivision_WhenFourIsDividedByTwo_ShouldReturnTwo() {
-        Calculator calculator = new Calculator();
         int result = calculator.integerDivision(4, 2);
         assertEquals(2, result, "Division did not produce expected result, 4 / 2 did not produce 2");
     }
@@ -29,7 +51,6 @@ class CalculatorTest {
     void integerSubtraction() {
         // AAA
         // Arrange(Given) -> Act(When) -> Assert(Then)
-        Calculator calculator = new Calculator();
         int subtrahend = 2;
         int minuend = 10;
         int expectedResult = 8;
