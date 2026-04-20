245f97fddb46f88038fbfa6a51f144e9ccb7a446
parom <parom.evgen@mail.ru>
2026-04-20 17:25:48 +0300

    28. @DisplayName annotation

--- src/test/java/com/parom/CalculatorTest.java
+++ src/test/java/com/parom/CalculatorTest.java
@@ -1,11 +1,14 @@
 package com.parom;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Test Math operations in Calculator class")
 class CalculatorTest {
 
+    @DisplayName("Test 4/2 = 2")
     // test<System Under Test>_<Condition or State Change>_<Expected Result>
     @Test
     void testIntegerDivision_WhenFourIsDividedByTwo_ShouldReturnTwo() {
@@ -14,12 +17,14 @@ class CalculatorTest {
         assertEquals(2, result, "Division did not produce expected result, 4 / 2 did not produce 2");
     }
 
+    @DisplayName("Division by zero")
     @Test
     void testIntegerDivision_WhenDividendIsDividedByZero_ShouldThrowArithmeticException(){
-
+        fail("Not implemented yet");
     }
 
     // TODO: Create a new JUnit Test method for integerSubtraction method.
+    @DisplayName("Test 10 - 2 = 8")
     @Test
     void integerSubtraction() {
         Calculator calculator = new Calculator();
