95fb2085f201d7b6e654853f4528194c2e896ddc
parom <parom.evgen@mail.ru>
2026-04-20 15:14:51 +0300

    21. Creating First Unit Test method

---
+++ src/main/java/com/parom/Calculator.java
@@ -0,0 +1,9 @@
+package com.parom;
+
+public class Calculator {
+
+    public int integerDivision(int dividend, int divisor) {
+        return dividend / divisor;
+    }
+
+}
---
+++ src/test/java/com/parom/CalculatorTest.java
@@ -0,0 +1,16 @@
+package com.parom;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void integerDivision() {
+        Calculator calculator = new Calculator();
+        int result = calculator.integerDivision(4, 2);
+        assertEquals(2, result);
+    }
+
+}
