dc19df5dcd9798d6dfebe0a8d50832cb2e7f5c88
parom <parom.evgen@mail.ru>
2026-04-20 15:51:49 +0300

    27. Naming Unit Tests

--- src/test/java/com/parom/CalculatorTest.java
+++ src/test/java/com/parom/CalculatorTest.java
@@ -6,19 +6,31 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
+    // test<System Under Test>_<Condition or State Change>_<Expected Result>
     @Test
-    void integerDivision() {
+    void testIntegerDivision_WhenFourIsDividedByTwo_ShouldReturnTwo() {
         Calculator calculator = new Calculator();
         int result = calculator.integerDivision(4, 2);
         assertEquals(2, result, "Division did not produce expected result, 4 / 2 did not produce 2");
     }
 
+    @Test
+    void testIntegerDivision_WhenDividendIsDividedByZero_ShouldThrowArithmeticException(){
+
+    }
+
     // TODO: Create a new JUnit Test method for integerSubtraction method.
     @Test
-    void integerSubtraction(){
-        Calculator  calculator = new Calculator();
-        int result = calculator.subtraction(10, 2);
-        assertEquals(8, result);
+    void integerSubtraction() {
+        Calculator calculator = new Calculator();
+        int subtrahend = 2;
+        int minuend = 10;
+        int expectedResult = 8;
+        int actualResult = calculator.subtraction(minuend, subtrahend);
+        assertEquals(expectedResult, actualResult,
+                () -> minuend + "-" + subtrahend + " did not produce " + expectedResult);
     }
+
+
 
 }
