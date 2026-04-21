8098d2f638455e42abd38189bbb89add6807dd23
parom <parom.evgen@mail.ru>
2026-04-21 12:27:00 +0300

    46. Changing Test Instance Lifecycle - example 1

--- src/test/java/com/parom/MethodOrderedByOrderIndexTest.java
+++ src/test/java/com/parom/MethodOrderedByOrderIndexTest.java
@@ -1,35 +1,46 @@
 package com.parom;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
+
+//@TestInstance(TestInstance.Lifecycle.PER_METHOD) // default configuration
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class MethodOrderedByOrderIndexTest {
+
+    StringBuilder completed = new StringBuilder("");
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("The state of instance object is: " + completed);
+    }
 
     @Order(1)
     @Test
     void testD() {
         System.out.println("Running test D");
+        completed.append("1");
     }
 
     @Order(3)
     @Test
     void testA() {
         System.out.println("Running test A");
+        completed.append("3");
     }
 
     @Order(2)
     @Test
     void testC() {
         System.out.println("Running test C");
+        completed.append("2");
     }
 
     @Order(4)
     @Test
     void testB() {
         System.out.println("Running test B");
+        completed.append("4");
     }
 
 }
