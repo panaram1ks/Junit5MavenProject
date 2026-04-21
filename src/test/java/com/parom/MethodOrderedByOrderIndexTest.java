4042906cd2aae9d184ccf25175ba6abe47aeeb00
parom <parom.evgen@mail.ru>
2026-04-21 12:00:33 +0300

    43. Methods Order - Random by order index

---
+++ src/test/java/com/parom/MethodOrderedByOrderIndexTest.java
@@ -0,0 +1,35 @@
+package com.parom;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class MethodOrderedByOrderIndexTest {
+
+    @Order(1)
+    @Test
+    void testD() {
+        System.out.println("Running test D");
+    }
+
+    @Order(3)
+    @Test
+    void testA() {
+        System.out.println("Running test A");
+    }
+
+    @Order(2)
+    @Test
+    void testC() {
+        System.out.println("Running test C");
+    }
+
+    @Order(4)
+    @Test
+    void testB() {
+        System.out.println("Running test B");
+    }
+
+}
